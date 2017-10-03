d2b85d6df77a81106da9c9d09c9eccfcf8a2af1a
Dima Krasnov <fl.krasnov@gmail.com>
2017-10-03 16:10:48 +0300

    feat: more dev preparings

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-class Header extends Component {
-  render() {
-    return <header>header</header>;
-  }
-}
+const Header = styled.header`
+  background: red;
+  color: #fff;
+`;
 
-export default Header;
+export default () => {
+  return (
+    <Header>
+      <a href="/">
+        <img src="" alt="Features I have Used and Loved logo" />
+      </a>
+    </Header>
+  );
+};
