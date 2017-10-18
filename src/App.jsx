e4e9fbca583498070a64306174386c27f18c55fc
Dima Krasnov <fl.krasnov@gmail.com>
2017-10-18 09:35:12 +0300

    feat: simple onepage scrolling example

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import Header from './Header';
-import Footer from './Footer';
-
-const Wrapper = styled.div`
-  position: relative;
-  background: #ececec;
-`;
-
-const Block1 = styled.div`
-  height: 600px;
-  background: #cac;
-`;
-
-const Block2 = styled.div`
-  height: 600px;
-  background: #aca;
-`;
-
-const Block3 = styled.div`
-  height: 600px;
-  background: #cca;
-`;
+import { ScrollSection, ScrollContainer } from 'react-onepage-scroll';
 
 class App extends Component {
   render() {
     return (
-      <Wrapper>
-        <Header />
-        <div>
-          <Block1 />
-        </div>
-        <Footer />
-      </Wrapper>
+      <ScrollContainer>
+        <ScrollSection
+          style={{
+            backgroundColor: '#cfb5cf'
+          }}
+          pageId={0}
+        />
+
+        <ScrollSection
+          style={{
+            backgroundColor: '#cfcfb5'
+          }}
+          pageId={1}
+        />
+
+        <ScrollSection
+          style={{
+            backgroundColor: '#b5cfcf'
+          }}
+          pageId={2}
+        />
+      </ScrollContainer>
     );
   }
 }
