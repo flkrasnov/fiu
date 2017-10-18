96f18ed240669e0a61856aa063f132f830721221
Dima Krasnov <fl.krasnov@gmail.com>
2017-10-18 11:38:39 +0300

    feat: components blocks added

---
+++ src/Slides/Summary/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+class Summary extends Component {
+  render() {
+    return <Wrap>Summary</Wrap>;
+  }
+}
+
+export default Summary;
